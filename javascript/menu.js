3a3ba6f907623d5cf18d353dc8686c8f2c35016c
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-23 17:13:46 +0300

    Add files via upload

---
+++ javascript/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/cybersecurity.html">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/tools/list.html">Εργαλεία</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/online-tools/list.html">Online Tools</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="#">Έγγραφα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
