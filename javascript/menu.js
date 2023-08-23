6c6414b04c6d93b77a7388b7cbb553ae026a9055
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-23 17:29:38 +0300

    Update menu.js

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    var menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/">Αρχική Σελίδα</a></li>';
+    var menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/">Αρχική Σελίδα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/tools/list.html">Εργαλεία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/online-tools/list.html">Online Tools</a></li>';
+    menuLink = ' <li><a href="https://github.com/GrCodeClub/cybersecurity/edit/main/website.html">Website</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="#">Έγγραφα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
