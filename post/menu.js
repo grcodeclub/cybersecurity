a156b1387b2fb04db28840210bf822b072b3d9ae
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-23 23:18:33 +0300

    Update menu.js

--- post/menu.js
+++ post/menu.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/">Κυβερνοασφάλεια</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/tools/list.html">Εργαλεία</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/post/tools/list.html">Εργαλεία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/website.html">Website</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/post/website.html">Website</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="#">Έγγραφα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity/post/ορολογία.html">Ορολογία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="https://grcodeclub.github.io/linux">Linux</a></li>';
     $('.sidebarMenuInner').append(menuLink);
