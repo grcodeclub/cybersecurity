d7ddbf1d13897e6f3146ee283570da9af86c7df1
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-10-29 23:14:03 +0200

    Update list_tools.sh

--- list_tools.sh
+++ list_tools.sh
@@ -2,6 +2,12 @@
 apt install nmap -y
 apt install crunch -y
 apt install wireshark-qt -y
+apt apt install john -y
+apt install ncat -y
+apt install burpsuite -y
+apt install hashcat -y
 apt install gobuster -y
 apt install binwalk -y
+apt install gobuster -y
 apt install libimage-exiftool-perl -y
+
