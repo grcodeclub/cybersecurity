ad690fba4e797be9a3b1e28a4c9bc8a6426efce3
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2023-10-31 17:33:25 +0200

    Create install-all.sh

---
+++ install/install-all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt install wget -y
+wget https://github.com/GrCodeClub/cybersecurity/blob/main/install/list_tools.sh
+wget https://github.com/GrCodeClub/cybersecurity/blob/main/install/install_wordlist.sh
+chmod +x ./list_tools.sh
+chmod +x ./install_wordlist.sh
+sudo ./list_tools.sh
+sudo ./install_wordlist.sh
