ab3bb09ff573249d3734ad3837f4bf447b359f94
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2023-10-31 17:30:22 +0200

    Update and rename wordlist.sh to install_wordlist.sh

---
+++ install/install_wordlist.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git clone https://github.com/danielmiessler/SecLists.git
+wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
--- install/wordlist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git clone https://github.com/danielmiessler/SecLists.git
-git clone https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
