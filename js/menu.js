cd8322f316d64dfe90b22d0b56ca8f05a467a671
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-04-27 19:28:20 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/tools">Εργαλεία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/attacks">Επιθέσεις</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/post">Έγγραφα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/orologia">Ορολογία</a></li>';
