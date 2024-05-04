d36411a2666ba254609edc0bdeee26c6ecad150b
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-05-04 20:46:45 +0300

    Delete install directory

--- install/install-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget https://github.com/GrCodeClub/cybersecurity/blob/main/install/list_tools.sh
-wget https://github.com/GrCodeClub/cybersecurity/blob/main/install/install_wordlist.sh
-chmod +x ./list_tools.sh
-chmod +x ./install_wordlist.sh
-sudo ./list_tools.sh
-sudo ./install_wordlist.sh
