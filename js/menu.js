0e83ed3defef2ad4bc309ebf23cfff7093f9a553
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-27 13:33:09 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,19 @@
-$(document).ready(function() {
-    var  menuLink = '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.gr/cybersecurity/">Κυβερνοασφάλεια</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/tools">Εργαλεία</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/attacks">Επιθέσεις</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/post">Έγγραφα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.gr/cybersecurity/orologia">Ορολογία</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.gr/linux">Linux</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-  });
+window.onload = function() {
+    var menuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.gr/cybersecurity/">Κυβερνοασφάλεια</a></li>',
+        '<li><a href="https://grcodeclub.gr/cybersecurity/tools">Εργαλεία</a></li>',
+        '<li><a href="https://grcodeclub.gr/cybersecurity/attacks">Επιθέσεις</a></li>',
+        '<li><a href="https://grcodeclub.gr/cybersecurity/post">Έγγραφα</a></li>',
+        '<li><a href="https://grcodeclub.gr/cybersecurity/orologia">Ορολογία</a></li>',
+        '<li><a href="https://grcodeclub.gr/linux">Linux</a></li>';
+        '<li style="height: 200px;"></li>';
+    ];
+    
+    var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+    $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+    menuLinks.forEach(function(link) {
+        $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+    });
+});
+};
