212b20ffba56a2ff10305b22ed23a72639c290a2
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-08-12 14:10:41 +0300

    Add files via upload

---
+++ install/list_tools.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt install nmap -y
+apt install crunch -y
+apt install wireshark-qt -y
+apt apt install john -y
+apt install ncat -y
+apt install burpsuite -y
+apt install hashcat -y
+apt install gobuster -y
+apt install binwalk -y
+apt install gobuster -y
+apt install libimage-exiftool-perl -y
+apt install ghidra -y
+apt-get install powershell -y
